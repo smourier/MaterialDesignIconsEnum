276be53b7f6cc7c569825fd5648e5f541d4663e4
Simon Mourier <smourier@users.noreply.github.com>
2025-01-04 12:37:25 +0100

    updated copyrights

--- MaterialDesignIconsEnum/Properties/AssemblyInfo.cs
+++ MaterialDesignIconsEnum/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: AssemblyCompany("Simon Mourier")]
 [assembly: AssemblyProduct("MaterialDesignIconsEnum")]
-[assembly: AssemblyCopyright("Copyright (C) 2016-2018 Simon Mourier. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (C) 2016-2025 Simon Mourier. All rights reserved.")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("20afc51f-948b-4b2d-9d04-4e7784983076")]
